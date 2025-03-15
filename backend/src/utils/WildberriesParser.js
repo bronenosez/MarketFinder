f832c1b7e1786af41209e29068f40a91fcbb8cfa
tiditik <69196938+tiditik@users.noreply.github.com>
2025-03-15 11:03:46 +0300

    Checked for the absence of the product on Wildberries

--- backend/src/utils/WildberriesParser.js
+++ backend/src/utils/WildberriesParser.js
@@ -1,3 +1,4 @@
+import ApiError from "./ApiError.js";
 import BaseParser from "./BaseParser.js";
 import fetch from "node-fetch";
 
@@ -8,23 +9,28 @@ class WildberriesParser extends BaseParser {
         `https://search.wb.ru/exactmatch/ru/common/v9/search?ab_testing=false&appType=1&curr=rub&dest=123589415&lang=ru&page=1&query=${productName}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false`
       );
       let data = await response.json();
-      let products = [];
 
-      for (let i = 0; i < data.data.products.length; i++) {
-        let item = {};
-        let part_number = data.data.products[i].id;
+      if (data.data.total !== 0) {
+        let products = [];
 
-        item.name = data.data.products[i].name;
-        item.price = data.data.products[i].sizes[0].price.total / 100;
-        item.link = `https://www.wildberries.ru/catalog/${part_number}/detail.aspx`;
-
-        products.push(item);
+        for (let i = 0; i < data.data.products.length; i++) {
+          let item = {};
+          let part_number = data.data.products[i].id;
+  
+          item.name = data.data.products[i].name;
+          item.price = data.data.products[i].sizes[0].price.total / 100;
+          item.link = `https://www.wildberries.ru/catalog/${part_number}/detail.aspx`;
+  
+          products.push(item);
+        }
+  
+        return products;
+      } else {
+        throw ApiError.badRequest("Товар не найден.");
       }
-
-      return products;
     } catch (error) {
       console.error("Ошибка:", error);
-      return [];
+      throw error;
     }
   }
 
@@ -37,15 +43,19 @@ class WildberriesParser extends BaseParser {
       let response = await fetch(url);
       let data = await response.json(url);
 
-      let item = {};
-      item.name = data.data.products[0].name;
-      item.price = data.data.products[0].priceU / 100;
-      item.link = productUrl;
-
-      return item;
+      if (data.data.products.length !== 0) {
+        let item = {};
+        item.name = data.data.products[0].name;
+        item.price = data.data.products[0].salePriceU / 100 || data.data.products[0].price / 100;
+        item.link = productUrl;
+  
+        return item;
+      } else {
+        throw ApiError.badRequest("Товар не найден.");
+      }
     } catch (error) {
       console.error("Ошибка:", error);
-      return [];
+      throw error;
     }
   }
 }
