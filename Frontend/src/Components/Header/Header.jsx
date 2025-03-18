9b5269d09be8cfa004963f7238b06d4a1d869b38
VladTarik <vladtarik4@gmail.com>
2025-03-18 20:34:00 +0300

    start creating mobile version

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+/* import {useState} from 'react';
 import "../../styles/Header.css";
 import '../../styles/index.css';
 import logo__basket from "../../img/logo__basket-svg.svg";
@@ -69,3 +69,112 @@ export default function Header () {
     )
 }
 
+ */
+
+import React, { useState, useEffect } from 'react';
+import "../../styles/Header.css";
+import '../../styles/index.css';
+import logo__basket from "../../img/logo__basket-svg.svg";
+import info from "../../img/info.svg";
+import RegistrationForm from "./RegistrationForm";
+import HowItWorks from "./HowItWorks";
+import LoginFormModal from './LoginForm';
+
+export default function Header() {
+    const [infoCondition, setInfoCondition] = useState(false);
+    const [registrationCondition, setRegistrationCondition] = useState(false);
+    const [loginCondition, setLoginCondition] = useState(false);
+    const [isMobile, setIsMobile] = useState(false); 
+
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia('(max-width: 1024px)'); 
+
+        const handleResize = (e) => {
+            setIsMobile(e.matches);
+        };
+
+        setIsMobile(mediaQuery.matches); 
+        mediaQuery.addEventListener('change', handleResize);
+
+        return () => {
+            mediaQuery.removeEventListener('change', handleResize);
+        };
+    }, []);
+
+    function handleInfoCondition() {
+        setInfoCondition((c) => (!c));
+    }
+
+    function handleRegistrationCondition() {
+        setRegistrationCondition((c) => (!c));
+    }
+
+    function handleLoginCondition() {
+        setLoginCondition((c) => (!c));
+    }
+
+    return (
+        <header>
+            <div className="container header_wrap">
+                <div className="logo">
+                    <img src={logo__basket} alt="" />
+                    <p>MarketFinder</p>
+                </div>
+
+                {/* Условный рендеринг для мобильной и десктопной версии */}
+                {isMobile ? (
+                    
+                    <div className="mobile-menu">
+                        <div className="burger">
+                            <div className="line"></div>
+                            <div className="line"></div>
+                            <div className="line"></div>
+                        </div>
+                        <div className="burger__menu">
+                            <button onClick={() => handleInfoCondition()}>Информация</button>
+                            <button onClick={() => handleLoginCondition()}>Войти</button>
+                            <button onClick={() => handleRegistrationCondition()}>Регистрация</button>
+                        </div>
+                        
+                    </div>
+                ) : (
+                    // Десктопная версия
+                    <div className="buttons">
+                        <div className="info">
+                            <img src={info} alt="" onClick={() => handleInfoCondition()} />
+                        </div>
+                        <div className="autorization">
+                            <div
+                                className="sign_in"
+                                onClick={() => handleLoginCondition()}
+                            >
+                                <p>Войти</p>
+                            </div>
+                            <div
+                                className="sign_up"
+                                onClick={() => handleRegistrationCondition()}
+                            >
+                                <p>Регистрация</p>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+
+            {/* Модальные окна */}
+            <HowItWorks
+                condition={infoCondition}
+                setCondition={() => handleInfoCondition()}
+            />
+            <RegistrationForm
+                condition={registrationCondition}
+                setCondition={() => handleRegistrationCondition()}
+            />
+            <LoginFormModal
+                condition={loginCondition}
+                setCondition={() => handleLoginCondition()}
+            />
+        </header>
+    );
+}
--- Frontend/src/Components/Search/SearchBar.jsx
+++ Frontend/src/Components/Search/SearchBar.jsx
@@ -1,13 +1,42 @@
+import {useRef, useState} from 'react';
 import '../../styles/index.css';
 import '../../styles/Search/SearchBar.css';
-import find from "../../img/find.svg"
+import find_img from "../../img/find.svg"
 
 export default function SearchBar () {
+    const searchRef = useRef();
+    const [searchValue, setSearchValue] = useState('');
+
+    function handleSearchValue (e) {
+      setSearchValue(e.target.value);
+    }
+
+    function handleRequest () {
+      const searchName = searchRef.current.value.trim();
+      
+      if (searchName === '') {
+        return 
+      }
+
+      console.log(searchName)
+    }
+
     return (
         <div className="container searchBar">
           <div className="bar">
-            <input type="text" placeholder='Название товара' />
-            <img src={find} alt="" />
+            <input 
+              ref={searchRef}
+              type="text"
+              placeholder='Название товара' 
+              value={searchValue}
+              onChange={handleSearchValue}
+            />
+
+            <img 
+              src={find_img}
+              alt=""
+              onClick={handleRequest} 
+            />
 
           </div>
         </div>
